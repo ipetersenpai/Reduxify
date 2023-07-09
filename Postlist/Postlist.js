9944bcf1478ebcc9e0a5c3a44f2d0b6db1bc8c13
Peter Francis Robante <robantepeterfran@gmail.com>
2023-07-09 17:11:38 +0800

    feat:add post

--- Postlist/Postlist.js
+++ Postlist/Postlist.js
@@ -1,13 +1,22 @@
-const { createStore } = require("redux");
+const { createStore, combineReducers } = require("redux");
 
 const initialState = {
   posts: [],
 };
 
+const userInitialState = {
+  users: [],
+};
+//action constant
+
+const addPost = "ADD_POST";
+const removePost = "REMOVE_POST";
+const addUser = "ADD_USER";
+
 //add a post
 const addPostaction = (post) => {
   return {
-    type: "ADD_POST",
+    type: addPost,
     payload: post,
   };
 };
@@ -15,28 +24,49 @@ const addPostaction = (post) => {
 //remove a post
 const removePostAction = (id) => {
   return {
-    type: "REMOVE_POST",
+    type: removePost,
     payload: id,
   };
 };
 
+const addUserAction = (user) => {
+  return {
+    type: addUser,
+    payload: user,
+  };
+};
 //reducer
 const postReducer = (state = initialState, action) => {
-  if (action.type === "ADD_POST") {
+  if (action.type === addPost) {
     return {
       posts: [...state.posts, action.payload],
     };
-  } else if (action.type === "REMOVE_POST") {
+  } else if (action.type === removePost) {
     return {
       posts: state.posts.filter((post) => {
         return post.id !== action.payload;
       }),
     };
+  } else {
+    return state;
   }
 };
 
+const userReducer = (state = userInitialState, action) => {
+  if (action.type === addUser) {
+    return {
+      users: [...state.users, action.payload],
+    };
+  }
+};
+
+const rootReducer = combineReducers({
+  posts: postReducer,
+  users: userReducer,
+});
+
 //store
-const store = createStore(postReducer);
+const store = createStore(rootReducer);
 
 //subscribe
 store.subscribe(() => {
@@ -59,4 +89,12 @@ store.dispatch(
     description: "yes it can fly now!",
   })
 );
-store.dispatch(removePostAction(1));
+store.dispatch(removePostAction(2));
+
+store.dispatch(
+  addUserAction({
+    userid: 1,
+    firstname: "Peter Francis",
+    lastnameL: "Robante",
+  })
+);
